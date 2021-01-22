bbea971cf64c9d2d0a7b6504d27cecc26a75fe27
Oliver <oj20783@gmail.com>
2021-01-22 18:49:53 +0000

    Creation of reading csv function

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import csv
+
+def csv_read(filename):
+    peopleList = []
+
+    myFile = open("Test Data/" + filename, mode = "r")
+
+    reader = csv.reader(myFile)
+
+    for each in reader:
+        peopleList.append(each)
+
+    myFile.close()
+
+    return(peopleList)
