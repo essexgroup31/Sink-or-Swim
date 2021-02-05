b7fbe7f83be8bd28b9ec05ade497388f6676ab8a
Oliver <oj20783@gmail.com>
2021-02-05 21:36:59 +0000

    Added fixdata as a callable file / function

--- fixdata.py
+++ fixdata.py
@@ -1,42 +1,41 @@
 from sklearn import impute
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 
-training_data = pd.read_csv("testdata/train.csv")
+def fixData(trainFileName, testFileName, imputer = "simple", strategy = "mean"):
 
-testing_data = pd.read_csv("testdata/test.csv")
+    training_data = pd.read_csv(trainFileName)
+    testing_data = pd.read_csv(testFileName)
 
-features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
-features2 = ["Embarked", "Sex"]
+    neededFeatures = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+    featuresForDummies = ["Embarked", "Sex"]
 
-y = training_data["Survived"]
-passengerID = testing_data["PassengerId"]
+    testSurvived = training_data["Survived"]
+    passengerID = testing_data["PassengerId"]
 
-training_data = training_data[features]
-testing_data = testing_data[features]
+    training_data = training_data[neededFeatures]
+    testing_data = testing_data[neededFeatures]
 
-tr_data = pd.get_dummies(training_data, columns=features2)
-te_data = pd.get_dummies(testing_data, columns=features2)
+    tr_data = pd.get_dummies(training_data, columns=featuresForDummies)
+    te_data = pd.get_dummies(testing_data, columns=featuresForDummies)
 
+    if imputer.lower() == "simple":
+        imp = impute.SimpleImputer(missing_values = np.NaN, strategy = strategy)
+    elif imputer.lower() == "knn":
+        imp = impute.KNNImputer(missing_values = np.NaN)
+    elif imputer.lower() == "iterative":
+        imp = impute.IterativeImputer(missing_values = np.NaN, initial_strategy = strategy)
+    else:
+        print("You did not enter a correct imputation method.")
+        print("Correct imputation methods include: \"Simple\", \"KNN\", \"Iterative\"")
+    
 
-imp = impute.SimpleImputer(missing_values=np.NaN, strategy = "median")
-# Mean median most_frequent constant
+    imp.fit(te_data)
+    dummied_test = imp.transform(te_data)
 
-imp.fit(te_data)
-dummied_test = imp.transform(te_data)
+    imp.fit(tr_data)
+    dummied_train = imp.transform(tr_data)
 
-imp.fit(tr_data)
-dummied_train = imp.transform(tr_data)
+    print("Data fixed.")
 
-
-clf = LogisticRegression(max_iter = 1000)
-
-clf.fit(dummied_train, y)
-
-survived = clf.predict(dummied_test)
-
-submission = pd.DataFrame({"PassengerId": passengerID, "Survived": survived})
-
-submission.to_csv("testdata/submission.csv", index = False)
-print("DONE! IDIOTS")
+    return(dummied_test, dummied_train, testSurvived, passengerID)
