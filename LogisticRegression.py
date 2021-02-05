d5bec91188c1c370ff3ca74faf09ffe375f4ba43
Oliver <oj20783@gmail.com>
2021-02-05 09:19:42 +0000

    Added Logistic regression and random forest.

---
+++ LogisticRegression.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+
+train_data = pd.read_csv("Test Data/train.csv")
+test_data = pd.read_csv("Test Data/test.csv")
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+
+X = pd.get_dummies(train_data[features])
+
+X_test = pd.get_dummies(test_data[features])
+
+y = train_data["Survived"]
+
+clf = LogisticRegression()
+
+clf.fit(X,y)
+
+survived = clf.predict(X_test)
+
+submission = pd.DataFrame({"PassengerId": test_data["PassengerId"], "Survived": survived})
+
+submission.to_csv("Test Data/submission.csv", index = False)
+print("File, submission.csv has been outputted.")
